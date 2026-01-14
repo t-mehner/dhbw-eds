e927887a642234c45c0b4ea0c06f91de4643ba44
Torben Mehner <torben.mehner@kit.edu>
2026-01-14 09:22:10 +0100

    Added PSE-empty

---
+++ PSE_empty/pong.vhd
@@ -0,0 +1,69 @@
+----------------------------------------------
+-- Statisches Pong 
+-- rechteckiger Ball
+----------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity PONG1 is
+   port(VIDEO_ON: in std_logic;
+        PIX_X,PIX_Y: in UNSIGNED(9 downto 0);
+        GRAPH_RGB: out std_logic_vector(11 downto 0) );
+end PONG1;
+
+architecture arch of PONG1 is
+   constant MAX_X: integer:=640;
+   constant MAX_Y: integer:=480;
+----------------------------------------------
+-- langer senkrechter Strich als Wand
+----------------------------------------------
+   constant WALL_X_L: integer:=32;
+   constant WALL_X_R: integer:=35;
+	constant WALL_RGB:  std_logic_vector(2 downto 0):= "000000001111"; -- Wandfarbe: Blau
+	signal   WALL_ON:   std_logic;
+	
+----------------------------------------------
+-- Bar
+----------------------------------------------
+
+----------------------------------------------
+-- Ball
+----------------------------------------------
+	
+----------------------------------------------
+-- hier geht es los
+----------------------------------------------   
+ begin
+----------------------------------------------
+-- Wand
+----------------------------------------------
+-- PIXEL innerhalb der WAND?
+   WALL_ON <=
+      '1' when (WALL_X_L<=PIX_X) and (PIX_X<=WALL_X_R) else
+      '0';
+
+----------------------------------------------
+-- Bar
+----------------------------------------------
+
+		
+----------------------------------------------
+-- Ball
+----------------------------------------------
+
+----------------------------------------------
+-- Anzeigeprozess
+----------------------------------------------
+   process(VIDEO_ON,WALL_ON)
+   begin
+      if VIDEO_ON='0' then
+          GRAPH_RGB <= "000000000000"; 			-- schwarzer Rahmen
+      else
+		if WALL_ON='1' then
+			GRAPH_RGB <= WALL_RGB;
+        else
+            GRAPH_RGB <= "111111110000"; 	   -- Hintergrund: Gelb
+         end if;
+      end if;
+   end process;
+end arch;
---
+++ PSE_empty/pong_top.vhd
@@ -0,0 +1,43 @@
+-- Top module for PONG
+-- static
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity PONG_STAT is
+   port (
+      CLK,RESET: in std_logic;
+      HSYNC, VSYNC: out  std_logic;
+      R, G, B: out std_logic_vector(3 downto 0)
+   );
+end PONG_STAT;
+
+architecture arch of PONG_STAT is
+   signal PIX_X, PIX_Y: UNSIGNED (9 downto 0);
+   signal VIDEO_ON, PIXEL_TICK: std_logic;
+   signal RGB_REG, RGB_NEXT: std_logic_vector(11 downto 0);
+begin
+   -- instantiate VGA sync
+   VGA_UNIT: entity work.VGA_SYNC
+      port map(CLK=>CLK, RESET=>RESET,
+               VIDEO_ON=>VIDEO_ON, p_tick=>PIXEL_TICK,
+               HSYNC=>HSYNC, VSYNC=>VSYNC,
+               PIX_X=>PIX_X, PIX_Y=>PIX_Y);
+   -- instantiate graphic 
+   PONG_UNIT: entity work.PONG1
+      port map (VIDEO_ON=>VIDEO_ON,
+                PIX_X=>PIX_X, PIX_Y=>PIX_Y,
+                GRAPH_RGB=>RGB_NEXT);
+   -- RGB buffer
+   process (CLK)
+   begin
+      if (CLK'event and CLK='1') then
+         if (PIXEL_TICK='1') then
+            RGB_REG <= RGB_NEXT;
+         end if;
+      end if;
+   end process;
+   R <= RGB_REG(11 downto 8);
+   G <= RGB_REG(7 downto 4);
+   B <= RGB_REG(3 downto 0);
+end arch;
